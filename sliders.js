76fafefbe1b2a676002e246d3acde60928874903
Fanis <gfanis1999@mail.ru>
2025-07-15 21:34:22 +0300

    comment10

--- sliders.js
+++ sliders.js
@@ -16,3 +16,43 @@ function slider() {
 }
 
 slider();
+
+// Inputs
+ $('input').on('input', function() {
+    var $label = $(this).prev('label');
+    if ($(this).val().trim() !== '') {
+      $label.addClass('input-title--active');
+    } else {
+      $label.removeClass('input-title--active');
+    }
+  });
+
+// Modal
+    $("[data-modal]").click(function () {
+        let modalId = $(this).attr("data-modal")
+
+        openModal(modalId)
+    })
+
+    function openModal(modalId){
+        $(`#${modalId}`).addClass("modal--active")
+        $("body").addClass("fixed-body")
+    }
+
+    $(".modal").click(function () {
+        closeModal()
+    })
+
+    $("._close-modal").click(function () {
+        closeModal()
+    })
+
+    $(".modal__dialog").click(function (e) {
+        e.stopPropagation()
+    })
+
+
+    function closeModal() {
+        $('.modal').removeClass("modal--active")
+        $("body").removeClass("fixed-body")
+    }
